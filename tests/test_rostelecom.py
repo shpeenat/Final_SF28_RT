b321218a98193fd8c690c92a9d8d52dc0e06adb3
shpeenat <106351624+shpeenat@users.noreply.github.com>
2022-11-14 06:20:48 +0500

    Update test_rostelecom.py

--- tests/test_rostelecom.py
+++ tests/test_rostelecom.py
@@ -2,12 +2,12 @@ from pagesRT.rt_page import AuthPage
 import time
 
 
-def test_contents_RT_page(driver):
+def test_EXP001_RT_page(driver):
     page = AuthPage(driver)
     page.driver.save_screenshot('RT_page.png')
 
 
-def test_RT_with_correct_data(driver):
+def test_EXP002_RT_with_correct_data(driver):
     page = AuthPage(driver)
     page.enter_email('makar_85@mail.ru')
     page.enter_pass('Makar1234')
@@ -16,7 +16,7 @@ def test_RT_with_correct_data(driver):
     page.driver.save_screenshot('RT_Makar85.png')
 
 
-def test_RT_with_incorrect_email(driver):
+def test_EXP003_RT_with_incorrect_email(driver):
     page = AuthPage(driver)
     page.enter_email('test@mail.ru')
     page.enter_pass('test1234')
@@ -24,7 +24,7 @@ def test_RT_with_incorrect_email(driver):
     page.driver.save_screenshot('RT_Incorrect_email.png')
 
 
-def test_RT_with_incorrect_pass(driver):
+def test_EXP004_RT_with_incorrect_pass(driver):
     page = AuthPage(driver)
     page.enter_email('makar_85@mail.ru')
     page.enter_pass('Makar2345')
@@ -32,50 +32,50 @@ def test_RT_with_incorrect_pass(driver):
     page.driver.save_screenshot('RT_Incorrect_PW.png')
 
 
-def test_RT_vk(driver):
+def test_EXP005_RT_vk(driver):
     page = AuthPage(driver)
     page.link_vk.click()
     page.driver.save_screenshot('RT_VK_link.png')
 
 
-def test_RT_ok(driver):
+def test_EXP006_RT_ok(driver):
     page = AuthPage(driver)
     page.link_ok.click()
     page.driver.save_screenshot('RT_OK_link.png')
 
 
-def test_RT_Mail(driver):
+def test_EXP007_RT_Mail(driver):
     page = AuthPage(driver)
     page.link_mail.click()
     page.driver.save_screenshot('RT_Mail_link.png')
 
 
-def test_RT_Google(driver):
+def test_EXP008_RT_Google(driver):
     page = AuthPage(driver)
     page.link_google.click()
     page.driver.save_screenshot('RT_Google_link.png')
 
 
-def test_RT_Ya(driver):
+def test_EXP009_RT_Ya(driver):
     page = AuthPage(driver)
     page.link_ya.click()
     page.driver.save_screenshot('RT_Ya_link.png')
 
 
-def test_RT_Cookies(driver):
+def test_EXP010_RT_Cookies(driver):
     page = AuthPage(driver)
     page.link_cookie.click()
     page.driver.save_screenshot('RT_Cookies.png')
 
 
-def test_RT_CP(driver):
+def test_EXP011_RT_CP(driver):
     page = AuthPage(driver)
     page.link_cp.click()
     time.sleep(1)
     page.driver.save_screenshot('RT_CP_link.png')
 
 
-def test_RT_register(driver):
+def test_EXP012_RT_register(driver):
     page = AuthPage(driver)
     page.link_reg.click()
     page.driver.save_screenshot('RT_register_link.png')
